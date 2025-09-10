f868cfd3f96b49a539f0ac79c4fb42ba36576510
MEDISETTY SURYA TEJA <161548584+suryateja000@users.noreply.github.com>
2025-09-10 07:03:02 +0530

    Updated

--- back-end/routes/leaderboardRoutes.js
+++ back-end/routes/leaderboardRoutes.js
@@ -1,10 +1,10 @@
-// routes/leaderboardRoutes.js
+
 import express from 'express';
 import { getLeaderboard } from '../controllers/leaderboardController.js';
 
 const router = express.Router();
 
-// GET /api/leaderboard
+
 router.get('/', getLeaderboard);
 
 export default router;
